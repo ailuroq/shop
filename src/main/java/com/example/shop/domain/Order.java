f474df54d91332bf4def846b6a436ac864c25ad5
ailuroq <yaroslav_ovsyannikov@list.ru>
2020-08-30 23:17:42 +0300

    db connections

--- src/main/java/com/example/shop/domain/Order.java
+++ src/main/java/com/example/shop/domain/Order.java
@@ -1,6 +1,8 @@
 package com.example.shop.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "orders")
@@ -9,5 +11,12 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
 }
